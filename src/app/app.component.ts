427b5fa46302bba298822d11a3e6fb4fe9a23608
Alex Gorbachov <alexpitronot@gmail.com>
2022-01-02 20:29:01 +0200

    2.1.2022

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  <h1>{{title}}</h1>
+  <h2>My favorite computer provider is: {{compProvider}}</h2>  
+  `
 })
+
 export class AppComponent {
-  title = 'computer-shop';
+  title: string;
+  compProvider: string;
+
+  constructor() {
+  this.title = `Computer Shop
+                  Welcome`;
+  this.compProvider = 'Asus'
+}
 }
