50a06bbed97ece457607bc5b22a87037e6b3810d
Alex Gorbachov <alexpitronot@gmail.com>
2022-01-05 17:01:32 +0200

    5.2.22

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,5 +16,11 @@ export class AppComponent {
   this.title = `Computer Shop
                   Welcome`;
   this.compProvider = 'Asus'
+  const events = [
+    {name: 'New Asus presentation', date: '3/1/2022', time: '10:00', location: {address: '23 Burla St', city: 'Haifa', country: 'Israel'}},
+    {name: 'New Gigabyte motherboard', date: '4/1/2022', time: '11:00', location: {address: '23 Burla St', city: 'Haifa', country: 'Israel'}},
+    {name: 'AMD experience', date: '1/11/2022', time: '13:00', location: {address: '123 Lilach St', city: 'Tel-Aviv', country: 'Israel'}},
+    {name: 'G5 modeling', date: '6/5/2022', time: '9:00', location: {address: '1 Histadrut St', city: 'Ashdod', country: 'Israel'}}
+  ]
 }
 }
