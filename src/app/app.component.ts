cfe3a3812cf88b940e92b434ef4c6b9c2b0ce429
Alex Gorbachov <alexpitronot@gmail.com>
2022-01-05 17:21:36 +0200

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,10 +17,10 @@ export class AppComponent {
                   Welcome`;
   this.compProvider = 'Asus'
   const events = [
-    {name: 'New Asus presentation', date: '3/1/2022', time: '10:00', location: {address: '23 Burla St', city: 'Haifa', country: 'Israel'}},
-    {name: 'New Gigabyte motherboard', date: '4/1/2022', time: '11:00', location: {address: '23 Burla St', city: 'Haifa', country: 'Israel'}},
-    {name: 'AMD experience', date: '1/11/2022', time: '13:00', location: {address: '123 Lilach St', city: 'Tel-Aviv', country: 'Israel'}},
-    {name: 'G5 modeling', date: '6/5/2022', time: '9:00', location: {address: '1 Histadrut St', city: 'Ashdod', country: 'Israel'}}
+    {name: 'New Asus presentation', date: '3/1/2022', time: '10:00', location: {address: '23 Burla St', city: 'Haifa', country: 'Israel', registration : true}},
+    {name: 'New Gigabyte motherboard', date: '4/1/2022', time: '11:00', location: {address: '23 Burla St', city: 'Haifa', country: 'Israel', registration : false}},
+    {name: 'AMD experience', date: '1/11/2022', time: '13:00', location: {address: '123 Lilach St', city: 'Tel-Aviv', country: 'Israel', registration : false}},
+    {name: 'G5 modeling', date: '6/5/2022', time: '9:00', location: {address: '1 Histadrut St', city: 'Ashdod', country: 'Israel', registration : true}}
   ]
 }
 }
