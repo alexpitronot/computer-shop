a58454a1fd9165ff320dd2dd1268a73bd6308fc2
Alex Gorbachov <alexpitronot@gmail.com>
2022-01-09 18:00:40 +0200

    9.01.22

--- src/app/event-details/event-details.component.ts
+++ src/app/event-details/event-details.component.ts
@@ -17,8 +17,8 @@ export class EventDetailsComponent implements OnInit {
   events = [
     {name: 'New Asus presentation', date: '3/1/2022', time: '10:00', location: {street: '23 Burla St', city: 'Haifa', country: 'Israel'}, registration : true, label: 'new'},
     {name: 'New Gigabyte motherboard', date: '4/1/2022', time: '11:00', location: {street: '23 Burla St', city: 'Haifa', country: 'Israel'}, registration : false, label: 'private'},
-    {name: 'AMD experience', date: '1/11/2022', time: '13:00', location: {street: '123 Lilach St', city: 'Tel-Aviv', country: 'Israel'}, registration : false, label: 'new'},
-    {name: 'AMD experience', date: '1/11/2022', location: {street: '123 Lilach St', city: 'Tel-Aviv', country: 'Israel'}, registration : false},
+    {name: 'AMD experience', date: '1/11/2022', time: '13:00', location: {street: '123 Lilach St', city: 'Tel-Aviv', country: 'Israel'}, registration : true, label: 'new'},
+    {name: 'Intel experience', date: '1/18/2022', location: {street: '12 Lilach St', city: 'Tel-Aviv', country: 'Israel'}, registration : false},
     {name: 'G5 modeling', date: '6/5/2022', time: '9:00', location: {street: '1 Histadrut St', city: 'Ashdod', country: 'Israel'}, registration : true, label: 'public'}
   ]
 
